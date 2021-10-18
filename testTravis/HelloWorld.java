66853a0876fa17264650f4c30abda63f32948209
Giuel <90395879+Giuel@users.noreply.github.com>
2021-10-18 16:16:58 +0200

    Implement Hello World

---
+++ testTravis/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package testTravis;
+
+/**
+ * @author giuelino
+ *
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!\n");
+	}
+}
